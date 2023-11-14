f2ae91e6a99990fd61c6f3bf4488388fcd3e0374
Mohammed Rasul <69099217+Rasul8215@users.noreply.github.com>
2023-11-14 22:09:49 +0530

    Resume updated latest

--- src/data/constants.js
+++ src/data/constants.js
@@ -7,9 +7,9 @@ export const Bio = {
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/Rasul8215?tab=repositories",
+  github: "https://github.com/Rasul8215",
   resume:
-    "https://drive.google.com/file/d/17XggebTGmxO74HEQNaYsArbLIeZjgKBB/view?usp=drivesdk",
+    "https://drive.google.com/file/d/1DVVXubTI3hFmNZTk2Vtq-fgwu6dTYhpP/view?usp=drivesdk",
   linkedin: "https://www.linkedin.com/in/mohammed-rasul-musthapa-529b4a22b",
 };
 
